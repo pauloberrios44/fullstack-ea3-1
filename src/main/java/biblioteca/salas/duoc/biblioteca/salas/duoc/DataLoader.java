33c499609b67f7b79e3137d22c7cbabefa8fed90
Paulo Andres Berrios Diaz <extpberrios@transbank.cl>
2025-05-28 21:35:01 -0400

    Avances programacion

--- src/main/java/biblioteca/salas/duoc/biblioteca/salas/duoc/DataLoader.java
+++ src/main/java/biblioteca/salas/duoc/biblioteca/salas/duoc/DataLoader.java
@@ -2,6 +2,7 @@ package biblioteca.salas.duoc.biblioteca.salas.duoc;
 
 import biblioteca.salas.duoc.biblioteca.salas.duoc.models.Carrera;
 import biblioteca.salas.duoc.biblioteca.salas.duoc.models.Estudiante;
+import biblioteca.salas.duoc.biblioteca.salas.duoc.models.Sala;
 import biblioteca.salas.duoc.biblioteca.salas.duoc.repository.*;
 import net.datafaker.Faker;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,7 +16,7 @@ import java.util.Random;
 
 @Profile("dev")
 @Component
-public class DataLoader {
+public class DataLoader implements CommandLineRunner {
 
     @Autowired
     private CarreraRepository carreraRepository;
@@ -23,10 +24,11 @@ public class DataLoader {
     @Autowired
     private EstudianteRepository estudianteRepository;
 
-    @Autowired SalaRepository salaRepository;
+    @Autowired
+    private SalaRepository salaRepository;
 
-    //@Override
-    public void main(String... args) throws Exception {
+    @Override
+    public void run(String... args) throws Exception {
         Faker faker = new Faker();
         Random rand = new Random();
 
@@ -44,13 +46,20 @@ public class DataLoader {
         for (int i = 0; i < 2500; i++) {
             Estudiante estudiante = new Estudiante();
             // estudiante.setId(i + 1);
-            estudiante.setRun(faker.idNumber().valid());
+            estudiante.setRun(faker.idNumber().valid().substring(0, 9));
             estudiante.setNombres(faker.name().fullName());
             estudiante.setCorreo(faker.internet().emailAddress());
             estudiante.setJornada(faker.options().option("D", "N").charAt(0));
             estudiante.setTelefono(faker.number().numberBetween(100000000, 999999999));
-            estudiante.setCodigoCarrera("123");
+            estudiante.setCodigoCarrera("123".substring(0,2));
             estudianteRepository.save(estudiante);
+        }
+
+        // Generar salas
+        for (int i = 0; i < 400; i++) {
+            Sala sala = new Sala();
+            sala.setNombre(faker.educator().campus());
+            sala.setCapacidad(45);
         }
     }
 
